91a176524bdcdfb2972bf3ac976384444482fc5b
unknown <dshipuk@yahoo.com>
2022-01-19 11:12:19 -0800

    finished tasks

--- index.js
+++ index.js
@@ -39,9 +39,41 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+// create new people
+
+const sallie = new Person('Sallie', 26)
+const mark = new Person('Mark', 23)
+const cori = new Person('Cori', 25)
+console.log(sallie.toString());
+console.log(mark.toString());
+console.log(cori.toString());
+
+mark.eat('pizza')
+mark.eat('tacos')
+mark.eat('sushi')
+mark.eat('burger')
+mark.eat('cake')
+mark.eat('noodles')
+console.log(mark.stomach)
+mark.poop()
+console.log(mark.stomach)
 
 
 
@@ -63,8 +95,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, mpg) {
+  this.model = model;
+  this.milesPerGallon = mpg
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons
 }
 
 
@@ -75,9 +113,28 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy
 }
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
+Baby.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+Baby.prototype.poop = function(){
+  this.stomach = [];
+}
+Baby.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+const alex = new Baby('al', 7, "Carrs")
+alex.eat("Fries")
+console.log(alex)
 
 
 /* 
